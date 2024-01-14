87a489fd42f217377680251e43840b8dd20db393
I3lackMarius <1993.marius@gmail.com>
2024-01-14 01:29:49 +0100

    updated last wrong image path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/dashboardNObg.png')",
+        "hero-pattern": "url('/assets/dashboardNObg.png')",
       },
     },
   },
